78c4e4a979fe4e433301e0ab5204706ee8fbb25a
schukark <schukark2004@gmail.com>
2023-08-15 19:37:52 +0300

    fixed Makefile

--- Makefile
+++ Makefile
@@ -1,10 +1,27 @@
-all: main
+#OBJS specifies which files to compile as part of the project
+OBJS = main.o classes\Board.o classes\Menu.o classes\GUI.o
 
-main: main.o GUI.o Menu.o Board.o
-	g++ -I src\include -Wall $^ -o $@ -L src\lib -lmingw32 -lSDL2main -lSDL2
+#CC specifies which compiler we're using
+CC = g++
+
+#COMPILER_FLAGS specifies the additional compilation options we're using
+# -w suppresses all warnings
+COMPILER_FLAGS = -Wall -lmingw32 -lSDL2main -lSDL2
+
+#LINKER_FLAGS specifies the libraries we're linking against
+LINKER_FLAGS = -L src\lib
+
+#OBJ_NAME specifies the name of our exectuable
+OBJ_NAME = main
+
+LIB_FLAGS = -I src\include
+
+#This is the target that compiles our executable
+all: $(OBJS)
+	$(CC) $(OBJS) $(LIB_FLAGS) $(LINKER_FLAGS) $(COMPILER_FLAGS) -o $(OBJ_NAME)
 
 %.o: %.cpp
-	g++ -I src\include -Wall -c -o $^ $@  -L src\lib -lmingw32 -lSDL2main -lSDL2
+	$(CC) $(LIB_FLAGS) $(COMPILER_FLAGS) $(LINKER_FLAGS) -c $^ -o $@
 
 clean:
-	del *.exe *.o all
+	del classes\*.o *.exe
--- main.cpp
+++ main.cpp
@@ -3,14 +3,12 @@
 #include <fcntl.h>
 #include <memory>
 
-#include "classes\Menu.cpp"
-#include "classes\Board.cpp"
-#include "classes\GUI.cpp"
+#include "SDL2\SDL.h"
 
 #define HEIGHT 480
 #define WIDTH 640
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     _setmode(_fileno(stdout), _O_U8TEXT);
 
     int grid_cell_size = 36;
@@ -31,14 +29,14 @@ int main(int argc, char **argv) {
 
     // The cursor ghost is a cursor that always shows in the cell below the
     // mouse cursor.
-    SDL_Rect grid_cursor_ghost = {grid_cursor.x, grid_cursor.y, grid_cell_size,
-                                  grid_cell_size};
+    SDL_Rect grid_cursor_ghost = { grid_cursor.x, grid_cursor.y, grid_cell_size,
+                                  grid_cell_size };
 
     // Dark theme.
-    SDL_Color grid_background = {22, 22, 22, 255}; // Barely Black
-    SDL_Color grid_line_color = {44, 44, 44, 255}; // Dark grey
-    SDL_Color grid_cursor_ghost_color = {44, 44, 44, 255};
-    SDL_Color grid_cursor_color = {255, 255, 255, 255}; // White
+    SDL_Color grid_background = { 22, 22, 22, 255 }; // Barely Black
+    SDL_Color grid_line_color = { 44, 44, 44, 255 }; // Dark grey
+    SDL_Color grid_cursor_ghost_color = { 44, 44, 44, 255 };
+    SDL_Color grid_cursor_color = { 255, 255, 255, 255 }; // White
 
     // Light Theme.
     // SDL_Color grid_background = {233, 233, 233, 255}; // Barely white
@@ -48,16 +46,16 @@ int main(int argc, char **argv) {
 
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Initialize SDL: %s",
-                    SDL_GetError());
+            SDL_GetError());
         return EXIT_FAILURE;
     }
 
-    SDL_Window *window;
-    SDL_Renderer *renderer;
+    SDL_Window* window;
+    SDL_Renderer* renderer;
     if (SDL_CreateWindowAndRenderer(window_width, window_height, 0, &window,
-                                    &renderer) < 0) {
+        &renderer) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION,
-                    "Create window and renderer: %s", SDL_GetError());
+            "Create window and renderer: %s", SDL_GetError());
         return EXIT_FAILURE;
     }
 
@@ -75,25 +73,25 @@ int main(int argc, char **argv) {
             case SDL_KEYDOWN:
                 switch (event.key.keysym.sym) {
 
-                    case SDLK_w:
-                    case SDLK_UP:
-                        grid_cursor.y -= grid_cell_size;
-                        break;
-                    
-                    case SDLK_s:
-                    case SDLK_DOWN:
-                        grid_cursor.y += grid_cell_size;
-                        break;
-                    
-                    case SDLK_a:
-                    case SDLK_LEFT:
-                        grid_cursor.x -= grid_cell_size;
-                        break;
-                    
-                    case SDLK_d:
-                    case SDLK_RIGHT:
-                        grid_cursor.x += grid_cell_size;
-                        break;
+                case SDLK_w:
+                case SDLK_UP:
+                    grid_cursor.y -= grid_cell_size;
+                    break;
+
+                case SDLK_s:
+                case SDLK_DOWN:
+                    grid_cursor.y += grid_cell_size;
+                    break;
+
+                case SDLK_a:
+                case SDLK_LEFT:
+                    grid_cursor.x -= grid_cell_size;
+                    break;
+
+                case SDLK_d:
+                case SDLK_RIGHT:
+                    grid_cursor.x += grid_cell_size;
+                    break;
                 }
 
                 break;
@@ -102,21 +100,21 @@ int main(int argc, char **argv) {
                 grid_cursor.x = (event.motion.x / grid_cell_size) * grid_cell_size;
                 grid_cursor.y = (event.motion.y / grid_cell_size) * grid_cell_size;
                 break;
-            
+
             case SDL_MOUSEMOTION:
                 grid_cursor_ghost.x = (event.motion.x / grid_cell_size) * grid_cell_size;
                 grid_cursor_ghost.y = (event.motion.y / grid_cell_size) * grid_cell_size;
 
                 mouse_active = SDL_TRUE;
                 break;
-            
+
             case SDL_WINDOWEVENT:
                 if (event.window.event == SDL_WINDOWEVENT_ENTER && !mouse_hover)
                     mouse_hover = SDL_TRUE;
                 else if (event.window.event == SDL_WINDOWEVENT_LEAVE && mouse_hover)
                     mouse_hover = SDL_FALSE;
                 break;
-            
+
             case SDL_QUIT:
                 quit = SDL_TRUE;
                 break;
@@ -125,12 +123,12 @@ int main(int argc, char **argv) {
 
         // Draw grid background.
         SDL_SetRenderDrawColor(renderer, grid_background.r, grid_background.g,
-                            grid_background.b, grid_background.a);
+            grid_background.b, grid_background.a);
         SDL_RenderClear(renderer);
 
         // Draw grid lines.
         SDL_SetRenderDrawColor(renderer, grid_line_color.r, grid_line_color.g,
-                            grid_line_color.b, grid_line_color.a);
+            grid_line_color.b, grid_line_color.a);
 
         for (int x = 0; x < 1 + grid_width * grid_cell_size; x += grid_cell_size) {
             SDL_RenderDrawLine(renderer, x, 0, x, window_height);
@@ -143,16 +141,16 @@ int main(int argc, char **argv) {
         // Draw grid ghost cursor.
         if (mouse_active && mouse_hover) {
             SDL_SetRenderDrawColor(renderer, grid_cursor_ghost_color.r,
-                                grid_cursor_ghost_color.g,
-                                grid_cursor_ghost_color.b,
-                                grid_cursor_ghost_color.a);
+                grid_cursor_ghost_color.g,
+                grid_cursor_ghost_color.b,
+                grid_cursor_ghost_color.a);
             SDL_RenderFillRect(renderer, &grid_cursor_ghost);
         }
 
         // Draw grid cursor.
         SDL_SetRenderDrawColor(renderer, grid_cursor_color.r,
-                            grid_cursor_color.g, grid_cursor_color.b,
-                            grid_cursor_color.a);
+            grid_cursor_color.g, grid_cursor_color.b,
+            grid_cursor_color.a);
         SDL_RenderFillRect(renderer, &grid_cursor);
 
         SDL_RenderPresent(renderer);
