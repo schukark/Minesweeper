447d591f2e266fae7d3ba9888a53ab42b64647f2
schukark <schukark2004@gmail.com>
2023-08-15 13:48:24 +0300

    SDL_Window setup

--- main.cpp
+++ main.cpp
@@ -2,14 +2,55 @@
 #include <io.h>
 #include <fcntl.h>
 #include <memory>
+#include <SDL2\SDL.h>
 
 #include "classes\Menu.cpp"
 #include "classes\Board.cpp"
 
+#define HEIGHT 480
+#define WIDTH 640
+
 int main(int argc, char **argv) {
     _setmode(_fileno(stdout), _O_U8TEXT);
-    std::unique_ptr<Menu> game(new Menu);
-    game->init_game();
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        std::wcout << "Failed to initialize SDL" << "\n";
+        std::wcout << "SDL2 Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("Minesweeper", 
+                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, HEIGHT, WIDTH, 0);
+    if (!window) {
+        std::wcout << "Couldn't create SDL window" << "\n";
+        std::wcout << "SDL2 Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+    if(!window_surface) {
+        std::wcout << "Failed to get the surface from the window\n";
+        std::wcout << "SDL2 Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+
+    bool keep_window_open = true;
+    while(keep_window_open) {
+        SDL_Event event;
+
+        while (SDL_PollEvent(&event) > 0) {
+            switch(event.type) {
+                case SDL_QUIT: {
+                    keep_window_open = false;
+                    break;
+                }
+            }
+            
+            SDL_UpdateWindowSurface(window);
+        }
+    }
+
+    //std::unique_ptr<Menu> game(new Menu);
+    //game->init_game();
 
     return 0;
 }
